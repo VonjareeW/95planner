cb706fb454bd960254534bb7f8816bd8304311f9
VonjareeW <52430595+VonjareeW@users.noreply.github.com>
2023-08-24 15:41:42 -0400

    made changes to time block layout and script

--- Assets /script.js
+++ Assets /script.js
@@ -2,7 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-const currentDay = dayjs().format('dddd, MMMM D, YYYY h:mm A');
+const currentDay = dayjs().format('dddd, MMMM D, YYYY');
 console.log(currentDay);
 
 const currentHr = dayjs().hour();
@@ -12,15 +12,42 @@ $(document).ready(function(){
 
   $("#currentDay").text(currentDay);
 
-  $(".saveBtn").on("click", function() {
+  $(".saveBtn").on("click", function(){
 
+    let timeBlockId = $(this).parent().attr("id");
+    let userInput = $(this).siblings(".description").val();
+    localStorage.setItem(timeBlockId, userInput);
 
+  });
+
+  //color change based on time of day
+  function eachTimeSlot() {
+    currentHr;
+    $(".time-block").each(function() {
+      let timeBlock = parseInt($(this).attr("id").split("hour"));
+
+      if (timeBlock < currentHr) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+      }
+
+      if (timeBlock === currentHr) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      }
+
+      if (timeBlock > currentHr) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      } 
+    })
   }
-}
 
 
-
-$("#hr-9  .description").val(localStorage.getItem("hr-9"));
+$("#hr-09 .description").val(localStorage.getItem("hr-09"));
 $("#hr-10 .description").val(localStorage.getItem("hr-10"));
 $("#hr-11 .description").val(localStorage.getItem("hr-11"));
 $("#hr-12 .description").val(localStorage.getItem("hr-12"));
@@ -30,7 +57,7 @@ $("#hr-15 .description").val(localStorage.getItem("hr-15"));
 $("#hr-16 .description").val(localStorage.getItem("hr-16"));
 $("#hr-17 .description").val(localStorage.getItem("hr-17"));
 
-
+ eachTimeSlot();
 //$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
